de66f31e7f2df679552d78f8a60784cec787d020
jgalloni <jgalloni91@gmail.com>
2017-01-11 12:30:51 -0300

    Revert "refactor version"
    This reverts commit acdd88c483f43370060d48e92e8b2ac3775733aa.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,64 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { OsdeSaludoAngular2RoutingModule } from './app-routing.module';
-import { RouterModule } from '@angular/router';
-import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
-import { myFirebaseConfig, myFirebaseAuthConfig } from '../environments/environment';
-import { MaterialModule } from '@angular/material';
-//import { SelectModule } from 'ng2-select/ng2-select';
-import { APP_BASE_HREF } from '@angular/common';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { NuevoProyectoComponent } from './nuevo-proyecto/nuevo-proyecto.component';
-import { MenuComponent } from './menu/menu.component';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LoginComponent } from './login/login.component';
-import { ProyectosComponent } from './proyectos/proyectos.component';
-import { PerfilUsuarioComponent } from './perfil-usuario/perfil-usuario.component';
-import { RegistroComponent } from './registro/registro.component';
-import { InformacionComponent } from './informacion/informacion.component';
-import { MenuFiltrosComponent } from './menu-filtros/menu-filtros.component';
-import { VerProyectoComponent } from './ver-proyecto/ver-proyecto.component';
-import { ProyectoWizardComponent } from './proyecto-wizard/proyecto-wizard.component';
-import { CalendarioComponent } from './calendario/calendario.component';
-import { MultiSelectComponent } from './multi-select/multi-select.component';
-import { WizardPaso1Component } from './proyecto-wizard/wizard-paso1/wizard-paso1.component';
-import { WizardPaso2Component } from './proyecto-wizard/wizard-paso2/wizard-paso2.component';
-import { WizardPaso3Component } from './proyecto-wizard/wizard-paso3/wizard-paso3.component'
-
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NuevoProyectoComponent,
-    MenuComponent,
-    LoginComponent,
-    NavbarComponent,
-    ProyectosComponent,
-    PerfilUsuarioComponent,
-    RegistroComponent,
-    InformacionComponent,
-    MenuFiltrosComponent,
-    VerProyectoComponent,
-    ProyectoWizardComponent,
-    CalendarioComponent,
-    MultiSelectComponent,
-    WizardPaso1Component,
-    WizardPaso2Component,
-    WizardPaso3Component,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    OsdeSaludoAngular2RoutingModule,
-    AngularFireModule.initializeApp(myFirebaseConfig, myFirebaseAuthConfig),
-    MaterialModule.forRoot(),
- //   SelectModule
+    HttpModule
   ],
-  providers: [OsdeSaludoAngular2RoutingModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import './polyfills.ts';
-import 'hammerjs'
-import 'firebase'
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
-
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
   enableProdMode();
