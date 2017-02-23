3105bf7e73ba64a26b57b015af6042a5c8028eb5
jgalloni <jgalloni91@gmail.com>
2017-02-23 13:12:10 -0300

    Version 1 del resumen de la carga

--- src/app/resumen/resumen.component.ts
+++ src/app/resumen/resumen.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Project} from '../model/project'
+import {ProjectDetail} from '../model/project-detail'
+import {ProyectoService} from '../services/proyecto.service'
 
 @Component({
   selector: 'app-resumen',
   templateUrl: './resumen.component.html',
-  styleUrls: ['./resumen.component.css']
+  styleUrls: ['./resumen.component.css'],
+  providers:[ProyectoService]
 })
 export class ResumenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private proyectoService:ProyectoService) { }
+
+  @Input()
+  public proyectoId:string;
+  @Input()
+  public resumen:ProjectDetail;
+
+  private proyecto:Project;
 
   ngOnInit() {
+    this.proyectoService.getById(this.proyectoId).subscribe(r=>this.proyecto=r);
+  }
+
+  public dateFromTo():string{
+    return this.resumen.dateFrom + "->" + this.resumen.dateTo;
   }
 
 }
